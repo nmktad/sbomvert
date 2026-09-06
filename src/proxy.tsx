906f5cb1a14cf9a88d8d38abfd17ba508dcc3815
Nathnael Tadesse <nmktad@gmail.com>
2026-09-06 15:26:38 +0200

    feat: add api key support for authenticating cli requests

--- src/proxy.tsx
+++ src/proxy.tsx
@@ -1,11 +1,34 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { FEATURE_FLAGS } from '@/lib/featureFlags'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+import { auth } from './lib/auth';
+
+export async function proxy(request: NextRequest) {
     if (!FEATURE_FLAGS.ENABLE_SCAN_API) {
         return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    const apiKeyString = request.headers.get("x-api-key");
+
+    if (!apiKeyString) {
+        return NextResponse.json(
+            { error: "Unauthorized: missing x-api-key header." }, 
+            { status: 401 }
+        );
+    }
+
+    // 2. Pass the extracted string into the function body layout
+    const result = await auth.api.verifyApiKey({
+        body: {
+            key: apiKeyString, 
+        },
+    });
+
+    if (!result || !result.valid) {
+        return NextResponse.json(
+            { error: "Unauthorized: Invalid or expired API key." }, 
+            { status: 401 }
+        );
     }
 
     return NextResponse.next()
